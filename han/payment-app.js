891400b88bf6f6ca3fe7608c824cb2f4a914d0de
barathstalin6 <83744700+barathstalin6@users.noreply.github.com>
2026-01-29 23:07:05 +0530

    Add files via upload

---
+++ han/payment-app.js
@@ -0,0 +1,112 @@
+(function() {
+  'use strict';
+
+  const BOBBUCKS_METHOD_URL = 'https://barathstalin6.github.io/summa/han/';
+  const PING_INTERVAL_MS = 60000;
+  const SW_MSG_APP_READY = 'payment_app_window_ready';
+  const SW_MSG_PING = 'ping';
+  const SW_MSG_UPDATE_MERCHANT = 'update_for_merchant';
+
+  let paymentRequestClient;
+  let methodData;
+
+  function addToInstrumentList(content, clickHandler) {
+    const linkedContainer = document.createElement('a');
+    linkedContainer.className = 'list-group-item';
+    linkedContainer.setAttribute('href', '#');
+    linkedContainer.appendChild(content);
+    linkedContainer.addEventListener('click', clickHandler);
+    document.getElementById('payment-instrument-list').appendChild(linkedContainer);
+  }
+
+  function populatePaymentInstrumentsList() {
+    // First, remove everything from the list. We won't bother with doing anything fancier.
+    const listNode = document.getElementById('payment-instrument-list');
+    while (listNode.lastChild) {
+      listNode.removeChild(listNode.lastChild);
+    }
+
+    if (methodData.some(method => method.supportedMethods.includes(BOBBUCKS_METHOD_URL))) {
+      // This merchant supports bobbucks, offer bobbucks balance as an option
+      const label = document.createElement('h4');
+      label.innerHTML = 'Pay with BobBucks balance ($50.00)';
+      addToInstrumentList(label, payWithBobBucksBalance);
+    }
+  }
+
+  function payWithBobBucksBalance() {
+    if (!paymentRequestClient) return;
+
+    const paymentAppResponse = {
+      methodName: BOBBUCKS_METHOD_URL,
+      details: {
+        bobbucks_token_id: 'ABCDEADBEEF',
+        message: 'Thanks for using BobBucks!',
+      },
+    };
+
+    paymentRequestClient.postMessage(paymentAppResponse);
+    window.close();
+  }
+
+  function handleServiceWorkerMessage(e) {
+    paymentRequestClient = e.source;
+
+    if (e.data.methodData) {
+      methodData = e.data.methodData;
+      document.getElementById('details').innerHTML = JSON.stringify(e.data, undefined, 2);
+      populatePaymentInstrumentsList();
+    } else if (e.data.updateWith) {
+      const log = document.getElementById('update-with-event-log');
+      log.innerHTML = `<b>Received update from merchant</b>: ${e.data.updateWith}`;
+      log.style.display = 'block';
+    }
+  }
+
+  function pingServiceWorkerToKeepItAlive() {
+    if (!navigator.serviceWorker.controller) {
+      console.error('Service Worker controller not found. Cannot send ping message.');
+      return;
+    }
+    navigator.serviceWorker.controller.postMessage(SW_MSG_PING);
+  }
+
+  function updateMerchant(evt) {
+    evt.preventDefault();
+    if (!navigator.serviceWorker.controller) {
+      console.error('Service Worker controller not found. Cannot send update merchant message.');
+      return;
+    }
+    navigator.serviceWorker.controller.postMessage(SW_MSG_UPDATE_MERCHANT);
+  }
+
+  function cancel() {
+    if (!paymentRequestClient) {
+      // payment request client may not have been set yet if the user cancels early.
+      // This is not an error, so we return silently.
+      return;
+    }
+    if (!navigator.serviceWorker.controller) {
+      console.error('Service Worker controller not found. Cannot send cancellation message.');
+      return;
+    }
+    paymentRequestClient.postMessage('The payment request is cancelled by user');
+    window.close();
+  }
+
+  function init() {
+    if (!navigator.serviceWorker.controller) {
+      console.error('Service Worker controller not found on init. Cannot send app ready message.');
+      return;
+    }
+
+    navigator.serviceWorker.addEventListener('message', handleServiceWorkerMessage);
+    navigator.serviceWorker.controller.postMessage(SW_MSG_APP_READY);
+    setInterval(pingServiceWorkerToKeepItAlive, PING_INTERVAL_MS);
+
+    document.getElementById('cancel').addEventListener('click', cancel);
+    document.getElementById('update-with-button').addEventListener('click', updateMerchant);
+  }
+
+  init();
+})();
---
+++ han/sw-bobbucks.js
@@ -0,0 +1,144 @@
+'use strict';
+
+// --- Constants for events and messages ---
+const CAN_MAKE_PAYMENT_EVENT = 'canmakepayment';
+const PAYMENT_REQUEST_EVENT = 'paymentrequest';
+const MESSAGE_EVENT = 'message';
+
+const MSG_PING = 'ping';
+const MSG_APP_WINDOW_READY = 'payment_app_window_ready';
+const MSG_UPDATE_FOR_MERCHANT = 'update_for_merchant';
+
+/**
+ * A utility class to resolve/reject a Promise from outside its constructor.
+ * This is useful for bridging event-driven APIs with Promises. In this service
+ * worker, we need to resolve the paymentrequest event's promise when we receive
+ * a 'message' from the payment app window.
+ */
+function PromiseResolver() {
+  /** @private {function(T=): void} */
+  this.resolve_;
+
+  /** @private {function(*=): void} */
+  this.reject_;
+
+  /** @private {!Promise<T>} */
+  this.promise_ = new Promise(function(resolve, reject) {
+    this.resolve_ = resolve;
+    this.reject_ = reject;
+  }.bind(this));
+}
+
+PromiseResolver.prototype = {
+  /** @return {!Promise<T>} */
+  get promise() {
+    return this.promise_;
+  },
+
+  /** @return {function(T=): void} */
+  get resolve() {
+    return this.resolve_;
+  },
+
+  /** @return {function(*=): void} */
+  get reject() {
+    return this.reject_;
+  },
+};
+
+// --- Global state for the current payment flow ---
+// We can only handle one payment request at a time.
+let currentPayment = {
+  paymentRequestEvent: null,
+  resolver: null,
+};
+
+self.addEventListener(CAN_MAKE_PAYMENT_EVENT, (e) => {
+  e.respondWith(true);
+});
+
+self.addEventListener(PAYMENT_REQUEST_EVENT, async (e) => {
+  // Stash the event and a resolver for later.
+  currentPayment.paymentRequestEvent = e;
+  currentPayment.resolver = new PromiseResolver();
+  e.respondWith(currentPayment.resolver.promise);
+
+  let url = 'https://barathstalin6.github.io/summa/han/';
+  // The methodData here represents what the merchant supports. We could have a
+  // payment selection screen, but for this simple demo if we see alipay in the list
+  // we send the user through the alipay flow.
+  if (e.methodData[0].supportedMethods[0].indexOf('alipay') !== -1) {
+    url += '/alipay.html';
+  }
+
+  try {
+    const windowClient = await e.openWindow(url);
+    if (windowClient === null) {
+      currentPayment.resolver.reject('Failed to open window');
+    }
+  } catch (err) {
+    currentPayment.resolver.reject(err);
+  }
+});
+
+self.addEventListener(MESSAGE_EVENT, (e) => {
+  // A message could be received before a payment request is initiated.
+  if (!currentPayment.resolver) {
+    return;
+  }
+
+  switch (e.data) {
+    case MSG_PING:
+      // Keep-alive message from the payment app window.
+      return;
+    case MSG_APP_WINDOW_READY:
+      sendPaymentRequestToClient();
+      return;
+    case MSG_UPDATE_FOR_MERCHANT:
+      sendUpdateToMerchant();
+      return;
+  }
+
+  if (e.data.methodName) {
+    currentPayment.resolver.resolve(e.data);
+  } else {
+    currentPayment.resolver.reject(e.data);
+  }
+});
+
+async function postMessageToClient(msg) {
+  // Note that the returned window_client from openWindow is not used since
+  // it might be changed by refreshing the opened page.
+  // Refer to https://www.w3.org/TR/service-workers-1/#clients-getall
+  const options = {
+    includeUncontrolled: false,
+    type: 'window',
+  };
+  const clientList = await clients.matchAll(options);
+  for (const client of clientList) {
+    // Might do more communications or checks to make sure the message is
+    // posted to the correct window only.
+    client.postMessage(msg);
+  }
+}
+
+function sendPaymentRequestToClient() {
+  if (!currentPayment.paymentRequestEvent) return;
+  // Copy the relevant data from the paymentRequestEvent to
+  // send to the payment app confirmation page.
+  // Note that the entire PaymentRequestEvent can not be passed through
+  // postMessage directly since it can not be cloned.
+  postMessageToClient({
+    total: currentPayment.paymentRequestEvent.total,
+    methodData: currentPayment.paymentRequestEvent.methodData,
+  });
+}
+
+async function sendUpdateToMerchant() {
+  if (!currentPayment.paymentRequestEvent) return;
+  // TODO(smcgruer): Implement properly, this is just testing communication.
+  const response = await currentPayment.paymentRequestEvent.changePaymentMethod('https://barathstalin6.github.io/summa/han/');
+  postMessageToClient({
+    updateWith: response.error,
+  });
+}
